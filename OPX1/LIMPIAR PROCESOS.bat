40c66d4e99744b8f767f98fd38e39e8cfae0c3f4
Rey <67885592+wdopax@users.noreply.github.com>
2026-01-27 23:13:44 -0600

    Update x2
    Update X

---
+++ OPX1/LIMPIAR PROCESOS.bat
@@ -0,0 +1,62 @@
+colo 1
+
+REM ******************** WINDOWS ********************
+
+REM Apaga todas as pastas temporárias e arquivos temporários do usuário
+takeown /A /R /D Y /F C:\Users\%USERNAME%\AppData\Local\Temp\
+icacls C:\Users\%USERNAME%\AppData\Local\Temp\ /grant administradores:F /T /C
+rmdir /q /s C:\Users\%USERNAME%\AppData\Local\Temp\
+md C:\Users\%USERNAME%\AppData\Local\Temp\
+
+REM Apaga os arquivos de \Windows\Temp
+takeown /A /R /D Y /F C:\windows\temp
+icacls C:\windows\temp /grant administradores:F /T /C
+rmdir /q /s c:\windows\temp
+md c:\windows\temp
+
+REM Apaga arquivos de log
+del c:\windows\logs\cbs\*.log
+del C:\Windows\Logs\MoSetup\*.log
+del C:\Windows\Panther\*.log /s /q
+del C:\Windows\inf\*.log /s /q
+del C:\Windows\logs\*.log /s /q
+del C:\Windows\SoftwareDistribution\*.log /s /q
+del C:\Windows\Microsoft.NET\*.log /s /q
+del C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\WebCache\*.log /s /q
+del C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\SettingSync\*.log /s /q
+del C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\Explorer\ThumbCacheToDelete\*.tmp /s /q
+del C:\Users\%USERNAME%\AppData\Local\Microsoft\"Terminal Server Client"\Cache\*.bin /s /q
+rmdir /q /s C:\Users\%USERNAME%\AppData\Local\Microsoft\Windows\INetCache\
+
+del /s /f /q c:\windows\temp\*.*
+rd /s /q c:\windows\temp
+md c:\windows\temp
+del /s /f /q C:\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+rd /s /q %temp%
+md %temp%
+deltree /y c:\windows\tempor~1
+deltree /y c:\windows\temp
+deltree /y c:\windows\tmp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+deltree /y c:\windows\spool\printers
+del c:\WIN386.SWP
+cls
+
+DEL /F /S /Q %HOMEPATH%\Config~1\Temp\*.* 
+
+DEL /F /S /Q C:\WINDOWS\Temp\*.* 
+DEL /F /S /Q C:\WINDOWS\Prefetch\*.* 
+DEL "%WINDIR%\Tempor~1\*.*" /F /S /Q 
+RD /S /Q "%HOMEPATH%\Config~1\Temp" 
+MD "%HOMEPATH%\Config~1\Temp" 
+RD /S /Q C:\WINDOWS\Temp\ 
+MD C:\WINDOWS\Temp 
+RD /S /Q C:\WINDOWS\Prefetch\ 
+MD C:\WINDOWS\Prefetch
+
+exit
+
